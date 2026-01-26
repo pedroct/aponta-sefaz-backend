36c883f3c5957dad0e09f9c39b235d9d0f20885f
Pedro Teixeira <pedro.teixeira@sefaz.ce.gov.br>
2026-01-26 05:58:46 -0300

    fix: corrigir cadeia de migrations alembic

--- alembic/versions/e5f6g7h8i9j0_create_organization_pats_table.py
+++ alembic/versions/e5f6g7h8i9j0_create_organization_pats_table.py
@@ -1,7 +1,7 @@
 """Create organization_pats table
 
 Revision ID: e5f6g7h8i9j0
-Revises: d4e5f6g7h8i9
+Revises: add_organizacao_col
 Create Date: 2026-01-26
 
 """
@@ -19,7 +19,7 @@ from app.config import get_settings
 
 # revision identifiers, used by Alembic.
 revision: str = 'e5f6g7h8i9j0'
-down_revision: Union[str, None] = 'd4e5f6g7h8i9'
+down_revision: Union[str, None] = 'add_organizacao_col'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
